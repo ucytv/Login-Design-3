8d296df73d3a2bdd26dcc6695e0ec64668342509
ugur can yaman <37077627+ucytv@users.noreply.github.com>
2020-05-23 06:12:25 +0300

    Login-Design-3

---
+++ app/src/main/java/com/ucy/tinydbapp/MainActivity.java
@@ -0,0 +1,21 @@
+package com.ucy.tinydbapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //Must be fullscreen!
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();//This is important!
+    }
+}
